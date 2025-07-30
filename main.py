aaa248d14bbb6ece4bdb71811e5514486778d7c9
allison2368 <allisonpeng10@gmail.com>
2025-07-30 16:24:13 -0700

    added data through kaggle api

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# main training model for emotion recognition 
+import kaggle 
+import pandas as pd
+# /Users/allisonpeng/.kaggle/kaggle.json
+
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files('tapakah68/facial-emotion-recognition', path='data', unzip=True)
+
+# Constants
+IMG_SIZE = 48
+DATA_DIR = '/kaggle/input/facial-emotion-recognition/images'
+EMOTIONS = ['Anger', 'Contempt', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprised']
+
+# Load the dataset
+csv_path = '/kaggle/input/facial-emotion-recognition/emotions.csv'
+emotions_df = pd.read_csv(csv_path)
+
